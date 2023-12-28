6108f0fd0bdff738ba2fcc33541b1d271b6e83a3
Bart Thate <bthate@dds.nl>
2023-12-28 15:27:35 +0100

    add mods

---
+++ mods/cmd.py
@@ -0,0 +1,15 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C0116,E0402,E0401,W0105
+
+
+"list of commands"
+
+
+from obj  import getmain
+
+Command = getmain("Command")
+
+
+def cmd(event):
+    event.reply(",".join(sorted(Command.cmds)))
